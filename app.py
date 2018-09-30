2aa7ce096698dcfe64ca2f5e2e902d113e2ce950
frankiev2 <34050014+frankiev2@users.noreply.github.com>
2018-09-30 02:01:40 -0400

    Updated name var

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!, Me!"
+    return "Hello World!"
 
 
 @app.route('/<name>')
 def hello_name(name):
-    return "Hello {}!".format("name")
+    return "Hello, {}!".format(name)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
